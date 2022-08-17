59f8509dea7bea2f55e15fd7bb03fb9c8989469f
steupidly <103988335+steupidly@users.noreply.github.com>
2022-08-17 15:12:54 +0700

    Update daily_thomas.py

--- dags/daily_thomas.py
+++ dags/daily_thomas.py
@@ -14,15 +14,26 @@ with DAG('daily_thomas',
     start = DummyOperator(
         task_id='start'
     )    
-      
+
+    # ingest_daily
     ingest_orders = BashOperator(
         task_id='ingest_orders',
         bash_command="""python3 /root/airflow/dags/ingest/thomas/ingest_orders.py {{ execution_date.format('YYYY-MM-DD') }}"""
     )
 
+    ingest_order_details = BashOperator(
+        task_id='ingest_order_details',
+        bash_command="""python3 /root/airflow/dags/ingest/thomas/ingest_order_details.py {{ execution_date.format('YYYY-MM-DD') }}"""
+    )
+
+    # daily to data lake
     to_datalake_orders = BashOperator(
         task_id='to_datalake_orders',
         bash_command="""gsutil cp /root/output/thomas/orders/orders_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/thomas/staging/orders/"""
     )
-
+    to_datalake_order_details = BashOperator(
+        task_id='to_datalake_order_details',
+        bash_command="""gsutil cp /root/output/thomas/order_details/order_details_{{ execution_date.format('YYYY-MM-DD') }}.csv gs://digitalskola-de-batch7/thomas/staging/order_details/"""
+    )
     start >> ingest_orders >> to_datalake_orders
+    start >> ingest_order_details >> to_datalake_order_details
